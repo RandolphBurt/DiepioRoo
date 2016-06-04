332d0855bdce512037d943f31e3ea333776101d7
RandolphBurt <randolph.burt@gmail.com>
2016-06-04 11:40:10 +0100

    Add player2 bullets.

--- Player.ts
+++ Player.ts
@@ -4,29 +4,57 @@ class Player {
     private _forward:Phaser.Key;
     private _left:Phaser.Key;
     private _right:Phaser.Key;
+    private _fire:Phaser.Key;
     private _game: Phaser.Game;
+    private bullets :Phaser.Group;
+    private bulletTime :number = 0;
 
-    constructor(game: Game, x: number, y: number, image: string, forward: Key, left: Key, right: Key) {
+    constructor(game: Game, x: number, y: number, playerImage: string, bulletImage: string, forward: Key, left: Key, right: Key, fire: Key) {
         this._game = game;
         this._forward = forward;
         this._left = left;
         this._right = right;
-        this._player = game.add.sprite(x, y, image);
+        this._fire = fire;
+        this._player = game.add.sprite(x, y, playerImage);
         this._player.anchor.setTo(0.5, 0.5);
 
         game.physics.enable(this._player, Phaser.Physics.ARCADE);
 
         this._player.body.drag.set(100);
         this._player.body.maxVelocity.set(200);
-    }
-    
+
+        this.bullets = this._game.add.group();
+        this.bullets.enableBody = true;
+        this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+        this.bullets.createMultiple(40, bulletImage);
+        this.bullets.setAll('anchor.x', 0.5);
+        this.bullets.setAll('anchor.y', 0.5);
+    };
+
     public get sprite() {
         return this._player;
     }
+    
+    public get bulletsGroup() {
+        return this.bullets;
+    }
+
+    private fireBullet = () => {
+        if (this._game.time.now > this.bulletTime) {
+            var bullet = this.bullets.getFirstExists(false);
+
+            if (bullet) {
+                bullet.reset(this._player.body.x + 40, this._player.body.y + 22);
+                bullet.lifespan = 2000;
+                bullet.rotation = this._player.rotation;
+                this._game.physics.arcade.velocityFromRotation(this._player.rotation, 400, bullet.body.velocity);
+                this.bulletTime = this._game.time.now + 150;
+            }
+        }
+    };
 
     public handleMovement = () => {
         if (this._forward.isDown) {
-            // TODO: We take away a radian because our graphics face up instead of to the right...  (Ideally change the graphics)
             this._game.physics.arcade.accelerationFromRotation(this._player.rotation, 200, this._player.body.acceleration);
         } else {
             this._player.body.acceleration.set(0);
@@ -38,6 +66,11 @@ class Player {
             this._player.body.angularVelocity = 300;
         } else {
             this._player.body.angularVelocity = 0;
+        }
+
+        if (this._fire.isDown)
+        {
+            this.fireBullet();
         }
     };
 }
--- gameState.ts
+++ gameState.ts
@@ -8,12 +8,9 @@ class GameState {
         this.game = new Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, update: this.update });
     }
 
-    private game: Game;
-    private player1: Player;
-    private player2: Player;
-
-    private bullets:Phaser.Group;
-    private bulletTime:number = 0;
+    private game :Game;
+    private player1 :Player;
+    private player2 :Player;
 
     private screenWrap = (sprite: Sprite) => {
         if (sprite.x < 0) {
@@ -31,24 +28,11 @@ class GameState {
         }
     };
 
-    private fireBullet = (player: Player) => {
-        if (this.game.time.now > this.bulletTime) {
-            var bullet = this.bullets.getFirstExists(false);
-
-            if (bullet) {
-                bullet.reset(player.sprite.body.x + 40, player.sprite.body.y + 22);
-                bullet.lifespan = 2000;
-                bullet.rotation = player.sprite.rotation;
-                this.game.physics.arcade.velocityFromRotation(player.sprite.rotation, 400, bullet.body.velocity);
-                this.bulletTime = this.game.time.now + 150;
-            }
-        }
-    }
-
     public preload = () => {
         this.game.load.image('player1', 'assets/player1.png');
-        this.game.load.image('player2', 'assets/player1.png'); // TODO: Separate iamge for player 2
-        this.game.load.image('bullet', 'assets/bullet.png');
+        this.game.load.image('player2', 'assets/player1.png'); // TODO: Separate image for player 2
+        this.game.load.image('bullet1', 'assets/bullet1.png');
+        this.game.load.image('bullet2', 'assets/bullet2.png');
     };
 
     public create = () => {
@@ -61,27 +45,25 @@ class GameState {
             200,
             150,
             'player1',
+            'bullet1',
             this.game.input.keyboard.addKey(Phaser.Keyboard.W),
             this.game.input.keyboard.addKey(Phaser.Keyboard.A),
-            this.game.input.keyboard.addKey(Phaser.Keyboard.D));
+            this.game.input.keyboard.addKey(Phaser.Keyboard.D),
+            this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR));
 
         this.player2 = new Player(
             this.game,
             400,
             300,
             'player2',
+            'bullet2',
             this.game.input.keyboard.addKey(Phaser.Keyboard.UP),
             this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT),
-            this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT));
-
-        this.bullets = this.game.add.group();
-        this.bullets.enableBody = true;
-        this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
-        this.bullets.createMultiple(40, 'bullet');
-        this.bullets.setAll('anchor.x', 0.5);
-        this.bullets.setAll('anchor.y', 0.5);
+            this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT),
+            this.game.input.keyboard.addKey(Phaser.Keyboard.END));
 
         this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        this.game.input.keyboard.addKey(Phaser.Keyboard.END);
     };
 
     public update = () => {
@@ -91,11 +73,7 @@ class GameState {
         this.screenWrap(this.player1.sprite);
         this.screenWrap(this.player2.sprite);
 
-        if (this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))
-        {
-            this.fireBullet(this.player1);
-        }
-
-        this.bullets.forEachExists(this.screenWrap, this);
+        this.player1.bulletsGroup.forEachExists(this.screenWrap, this);
+        this.player2.bulletsGroup.forEachExists(this.screenWrap, this);
     };
 }
